105257246363c10597cbf51b1ee611dee63287ec
Kyle Szombathy <kylemail2003@gmail.com>
2016-09-27 15:49:41 -0700

    Implement moving interface

---
+++ src/com/mycompany/a1/IMovingObject.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mycompany.a1;
+
+/**
+ * @author Kyle Szombathy
+ *
+ */
+public interface IMovingObject {
+    
+    public void move();
+    
+    public int getSpeed();
+    
+    public int setSpeed(int speed);
+    
+    public int getDirection();
+    
+    public int setDirection(int direciton);
+    
+}
